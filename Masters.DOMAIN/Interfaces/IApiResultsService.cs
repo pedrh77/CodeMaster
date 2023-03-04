e1bc4377b5285c4249bb89d59563b92125827d1c
PedroSantos <pedrohtth65@gmail.com>
2023-03-04 00:04:30 -0300

    [feature] Adding new error treatment  for api requisition

---
+++ Masters.DOMAIN/Interfaces/IApiResultsService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masters.DOMAIN.Interfaces
+{
+    public interface IApiResultsService 
+    {
+        public void CheckResponseApi<T>(string function, ApiResponse<T> retorno);
+    }
+}
---
+++ Masters.SERVICES/Services/DAL/ApiResultsService.cs
@@ -0,0 +1,16 @@
+﻿using Masters.DOMAIN.Interfaces;
+using Refit;
+
+namespace Masters.SERVICES.Services.DAL
+{
+    public class ApiResultsService : IApiResultsService
+    {
+        public void CheckResponseApi<T>(string function, ApiResponse<T> retorno)
+        {
+            if (!retorno.IsSuccessStatusCode)
+            {
+                throw new Exception($"[{function}]: Erro: {retorno.Error} Status Code: {retorno.StatusCode} Message: {retorno.Error.Content}");
+            }
+        }
+    }
+}
